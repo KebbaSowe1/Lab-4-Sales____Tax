9917a8d00d2a1c45c3bb958cff681dc858881f8e
Kebba <kebba.sowe24@gmail.com>
2025-02-17 22:36:51 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 200.54;
+        final double SALES_TAX_RATE = .05;
+        double salesTax = 0;
+        double totalPrice = 0;
+
+        System.out.println("You said the price is" + purchasePrice);
+
+        salesTax = purchasePrice + SALES_TAX_RATE;
+        totalPrice = salesTax + purchasePrice;
+
+        System.out.println("the sales tax is" + salesTax);
+        System.out.println("the total cost is" + totalPrice);
+
+        System.out.printf("Sales tax rounded is %6.2f", salesTax);
+    }
+}
